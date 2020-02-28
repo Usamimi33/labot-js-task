c8c2cd2c8d421b141b0d683122f9afbd64c7ba49
林奈央 <hayashinao@hayashinaonoMacBook-Pro.local>
2020-02-28 13:47:56 +0900

    ! = -1で期待動作確認

--- nabeatsu/nabeatsu.js
+++ nabeatsu/nabeatsu.js
@@ -1,6 +1,7 @@
 for (let num = 1; num <= 100; num++) {
     var str = String(num);
-    if (num % 3 === 0 || str.indexOf("3")) {
+
+    if (num % 3 === 0 || str.indexOf("3") != -1) {
         console.log(num + "!");
     }else{
         console.log(num);
