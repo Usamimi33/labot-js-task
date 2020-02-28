2caf237d957be6053e2f442671ed5fc3c31260ec
林奈央 <hayashinao@hayashinaonoMacBook-Pro.local>
2020-02-28 14:55:14 +0900

    varをconstに変更

--- nabeatsu/nabeatsu.js
+++ nabeatsu/nabeatsu.js
@@ -1,5 +1,5 @@
 for (let num = 1; num <= 100; num++) {
-    var str = String(num);
+    const str = String(num);
 
     if (num % 3 === 0 || str.indexOf("3") != -1) {
         console.log(num + "!");
